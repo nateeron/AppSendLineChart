12added90e4a3b7ba06feaeecf9fb73aab24163e
cayoshi <nateeronron@gmail.com>
2024-07-03 15:21:49 +0700

    ./setup_chrome.sh

--- setup_chrome.sh
+++ setup_chrome.sh
@@ -16,6 +16,7 @@ else
   echo "...Using Chrome from cache"
 fi
 
+gunicorn app:app
 # be sure to add Chromes location to the PATH as part of your Start Command
 # export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
 
